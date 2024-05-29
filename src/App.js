237690d72300e49b7d2914bbefab092f9870c2af
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-05-29 16:58:20 +0200

    🎉 init landing page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bitcoin Wildlife Conservatorium</h1>
+        <p>🐺 + 🐈</p>
+        <p>Stay tuned 👀</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
